829935bec31286c31e764fbf6fc7c96b2d944c2c
Andrew Daniel <andydotdaniel@gmail.com>
2021-02-17 22:50:35 +0700

    Add models for traces app

--- traces/models.py
+++ traces/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    phone_number = models.CharField(max_length=200, unique=True)
+    date_created = models.DateTimeField()
+
+class TracePoint(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    input_title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Entry(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    trace_point = models.ForeignKey(TracePoint, on_delete=models.RESTRICT)
+    value = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.value
+
+class Visit(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date_created = models.DateTimeField()
