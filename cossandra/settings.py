063c1f91922979c13cf6c7cb3c0f7d3324e3e484
Andrew Daniel <andydotdaniel@gmail.com>
2021-02-17 22:52:49 +0700

    Add traces app to list of installed apps

--- cossandra/settings.py
+++ cossandra/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'traces.apps.TracesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
