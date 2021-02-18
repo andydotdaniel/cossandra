3d38c6f311ce53bc6c14ed92bcecf7772cf802c1
Andrew Daniel <andydotdaniel@gmail.com>
2021-02-19 00:30:35 +0700

    Add entry form view for trace point

---
+++ traces/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'traces'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('entry/', views.entry, name='entry'),
+]
--- traces/views.py
+++ traces/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import TracePoint
+
+def index(request):
+    return HttpResponse("Traces Index")
+
+def entry(request):
+    questions = TracePoint.objects.all()
+    return render(request, 'traces/entry_form.html', { 'questions': questions })
