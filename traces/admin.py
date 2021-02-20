188484904b26b0f83bff96f7d837a1b551fd18c9
Andrew Daniel <andydotdaniel@gmail.com>
2021-02-20 12:54:27 +0700

    Add export action to admin panel for visits

--- traces/admin.py
+++ traces/admin.py
@@ -1,6 +1,19 @@
+from django.http import StreamingHttpResponse
 from django.contrib import admin
-
 from .models import Question, Visit
+from .exporter import Exporter
 
 admin.site.register(Question)
-admin.site.register(Visit)
+
+def export_to_file(modeladmin, request, queryset):
+    exporter = Exporter()
+    response = StreamingHttpResponse(exporter.generate_rows(queryset),
+                                     content_type="text/csv")
+    response['Content-Disposition'] = 'attachment; filename="visit_list.csv"'
+    return response
+export_to_file.short_description = "Export visits to file"
+
+class VisitAdmin(admin.ModelAdmin):
+    actions = [export_to_file]
+
+admin.site.register(Visit, VisitAdmin)
---
+++ traces/exporter.py
@@ -0,0 +1,38 @@
+import csv
+from .models import Question
+
+class Echo:
+    """An object that implements just the write method of the file-like
+    interface.
+    """
+    def write(self, value):
+        """Write the value by returning it, instead of storing in a buffer."""
+        return value
+
+class Exporter:
+    def __get_headers(self):
+        headers = ["phone_number"]
+        questions = Question.objects.all()
+        for question in questions:
+            headers.append(question.name)
+
+        return headers
+
+    def __format_row(self, queryset):
+        row = [queryset.customer.phone_number]
+
+        entry_inputs = queryset.customer.entry_set.all()
+        for entry_input in entry_inputs:
+            row.append(entry_input.value)
+
+        return row
+
+    def generate_rows(self, queryset):
+        """A view that streams a large CSV file."""
+        pseudo_buffer = Echo()
+        writer = csv.writer(pseudo_buffer)
+
+        yield writer.writerow(self.__get_headers())
+
+        for query in queryset:
+            yield writer.writerow(self.__format_row(query))
